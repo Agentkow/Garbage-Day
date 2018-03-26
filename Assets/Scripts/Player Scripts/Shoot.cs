c8554108c892fc1603af56a161a7f1b3fc50e26e
Agentkow <kennethkhau212@gmail.com>
2018-03-26 18:17:08 -0500

    sound

--- Assets/Scripts/Player Scripts/Shoot.cs
+++ Assets/Scripts/Player Scripts/Shoot.cs
@@ -6,6 +6,7 @@ public class Shoot : MonoBehaviour {
     public int playerNum = 1;
     public Rigidbody bag;
     public Transform fireTransform;
+    public AudioSource tossSound;
 
     public float force = 5.95f;
     public float coolDown = 0.1f;
@@ -19,6 +20,7 @@ public class Shoot : MonoBehaviour {
     {
         fireButton = "Fire" + playerNum;
         canFire = true;
+        tossSound = GetComponent<AudioSource>();
 
 	}
 
@@ -45,6 +47,7 @@ public class Shoot : MonoBehaviour {
         Rigidbody instance = Instantiate(bag, fireTransform.position, fireTransform.rotation);
         
         instance.velocity = force * fireTransform.forward;
+        tossSound.Play();
         
     }
 
